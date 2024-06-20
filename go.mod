91effcabb77eceef6c309422b39673edd789929b
tetsuya28 <n@tetsuya28.com>
2024-06-21 01:24:01 +0900

    init

---
+++ go.mod
@@ -0,0 +1,26 @@
+module tetsuya28/pgx-otel
+
+go 1.22.3
+
+require (
+	github.com/exaring/otelpgx v0.6.1
+	github.com/jackc/pgx/v5 v5.6.0
+	go.opentelemetry.io/contrib/propagators/b3 v1.27.0
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.27.0
+	go.opentelemetry.io/otel/sdk v1.27.0
+)
+
+require (
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/exaring/otelpgx"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func main() {
+	ctx := context.Background()
+
+	exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		panic(err)
+	}
+
+	opts := []sdktrace.TracerProviderOption{
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+	}
+	tp := sdktrace.NewTracerProvider(opts...)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(
+		b3.New(b3.WithInjectEncoding(b3.B3MultipleHeader)),
+	)
+
+	tracer := otel.Tracer("pgx-otel")
+	ctx, span := tracer.Start(ctx, "pgx-otel")
+	defer span.End()
+
+	url := "postgres://usr:pw@localhost:5432/db?sslmode=disable"
+	cfg, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.ConnConfig.Tracer = otelpgx.NewTracer()
+
+	conn, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	if err := conn.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	row, err := conn.Query(ctx, "SELECT 1")
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+
+	fmt.Println(row.RawValues())
+
+	if err := tp.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+}
